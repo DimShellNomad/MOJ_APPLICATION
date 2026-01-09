337d9932deabb7991752efb8a8bae356cb0c662a
DimShellNomad <25436493+DimShellNomad@users.noreply.github.com>
2026-01-09 09:57:35 +0000

    - Sorted out issue with reports not being sent to target directory

--- src/main/java/com/framework/utils/ExtentManager.java
+++ src/main/java/com/framework/utils/ExtentManager.java
@@ -3,13 +3,19 @@ package com.framework.utils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.File;
+
 public class ExtentManager {
     private static ExtentReports extent;
 
     public static ExtentReports getReportInstance() {
         if (extent == null) {
             // Path where report will be saved
-            ExtentSparkReporter spark = new ExtentSparkReporter("target/ExtentReport.html");
+            File path = new File("target");
+            if (!path.exists()) {
+                path.mkdirs();
+            }
+            ExtentSparkReporter spark = new ExtentSparkReporter("/app/target/ExtentReport.html");
             spark.config().setReportName("MOJ Project Test");
             spark.config().setDocumentTitle("Test Report");
 
